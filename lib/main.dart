4e3a14c6b257ccb929bc9f60ee784ceee61e68ba
معرض الاندلس <saedabbas0@gamil.com>
2022-03-29 20:44:11 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/Welcome/welcome_screen.dart';
+import 'package:flutter_auth/constants.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter Auth',
       theme: ThemeData(
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 240, 241, 147),
-      appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.amber,
-          title: Text("Software Engineering Department")),
-      body: SingleChildScrollView(
-        child: Container(
-            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            child: Column(
-              children: [
-                Text("student enrollment"),
-                Image.network(
-                  'https://cdn-icons-png.flaticon.com/512/146/146031.png',
-                  width: 70,
-                ),
-                Divider(
-                  color: Color.fromARGB(255, 172, 170, 170),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    icon: Icon(Icons.person),
-                    labelText: "User Name",
-                  ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: "First Name",
-                      // prefixIcon: Icon(Icons.person),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20))),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    //icon: Icon(Icons.person),
-                    labelText: "Last Name",
-                  ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: " Password",
-                    // prefixIcon: Icon(Icons.person),
-                  ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: " @ Email",
-                    //prefixIcon: Icon(Icons.person),
-                  ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Phone Number",
-                    prefixIcon: Icon(
-                      Icons.phone,
-                      color: Colors.blue,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-              ],
-            )),
-      ),
+      home: WelcomeScreen(),
     );
   }
 }
